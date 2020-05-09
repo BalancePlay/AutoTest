6b134d642ede70cca9f0091e543b3e9dfff4c7fb
Rajit Paul <shotdead09@gmail.com>
2020-05-09 15:16:16 +0530

    Tenth

--- index.php
+++ index.php
@@ -2,24 +2,24 @@
 
 print 'Creating a Chatbot';
 
-print 'Three parts of a chatbot';
+print '\n Three parts of a chatbot';
 
-print 'First is Intent';
+print '\n First is Intent';
 
-print 'Second is Entities';
+print '\n Second is Entities';
 
-print 'Third is dialouge';
+print '\n Third is dialouge';
 
-print 'Letus create a chatbot now';
+print '\n Letus create a chatbot now';
 
-print 'First step is to create manual intents';
+print '\n First step is to create manual intents';
 
-print 'Greeting can be an intent';
+print '\n Greeting can be an intent';
 
-print 'Intent is the goal or intention of the user';
+print '\n Intent is the goal or intention of the user';
 
-print 'It is very important for the chatbot to understand the intent of the user';
+print '\n It is very important for the chatbot to understand the intent of the user';
 
-print 'Another intent can be goodnight';
+print '\n Another intent can be goodnight';
 
 ?>
